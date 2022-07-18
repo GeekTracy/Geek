6c4c9edb04e34cc89ce155438a3b03c0313ef5ee
unknown <kingdee@ZBBB2111C-0047.kingdee.gbl>
2022-07-18 19:18:48 +0800

    test

---
+++ tracy/src/main/java/com/geek/tracy/TracyApplication.java
@@ -0,0 +1,13 @@
+package com.geek.tracy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracyApplication.class, args);
+	}
+
+}
---
+++ tracy/src/test/java/com/geek/tracy/TracyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.tracy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
