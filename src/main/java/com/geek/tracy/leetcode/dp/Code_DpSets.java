a159cb31f01737a5e9c6832573ef696c7f8227e8
Mike <709830862@qq.com>
2023-06-27 22:52:07 +0800

    1186. Maximum Subarray Sum with One Deletion 删除一次得到子数组最大和

--- src/main/java/com/geek/tracy/leetcode/dp/Code_DpSets.java
+++ src/main/java/com/geek/tracy/leetcode/dp/Code_DpSets.java
@@ -1,5 +1,7 @@
 package com.geek.tracy.leetcode.dp;
 
+import org.junit.Test;
+
 /**
  * 动态规划 Dynamic Programming 集合
  * @Author Tracy
@@ -7,6 +9,19 @@ package com.geek.tracy.leetcode.dp;
  */
 public class Code_DpSets {
 
+
+    @Test
+    public void maximumSumTest() {
+        System.out.println("except num: 3");
+        System.out.println(maximumSum(new int[]{2,1,-2,-5,-2}));
+//        System.out.println("except num: 4");
+//        System.out.println(maximumSum(new int[]{1, -2, 0, 3}));
+//        System.out.println("except num: 3");
+//        System.out.println(maximumSum(new int[]{1,-2,-2,3}));
+//        System.out.println("except num: -1");
+//        System.out.println(maximumSum(new int[]{-1,-1,-1,-1}));
+
+    }
 
     /**
      * ----- Kadane's Algorithm 算法   -----
@@ -34,8 +49,29 @@ public class Code_DpSets {
      * 1186. Maximum Subarray Sum with One Deletion 删除一次得到子数组最大和
      */
     public int maximumSum(int[] arr) {
-        return 0;
+        int max = arr[0];
+        // 保存i节点开始的最大子数组和
+        int[] sumStartHere = new int[arr.length];
+        // 保存到i节点的最大子数组和
+        int[] sumEndHere = new int[arr.length];
+        // 从i节点开始的最大子数组和
+        sumStartHere[arr.length - 1] = arr[arr.length - 1];
+        for (int i = arr.length - 2; i >= 0; i--) {
+            sumStartHere[i] = Math.max(arr[i], sumStartHere[i + 1] + arr[i]);
+        }
+        // 到i节点的最大子数组和
+        sumEndHere[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            sumEndHere[i] = Math.max(sumEndHere[i - 1] + arr[i], arr[i]);
+            max = Math.max(max, sumEndHere[i]);
+        }
+        // 当去掉的那个数下标为i，则比较：max 与 sumStartHere[i] + sumEndHere[i] 的大小
+        for (int i = 1; i < arr.length - 1; i++) {
+            max = Math.max(max, sumStartHere[i + 1] + sumEndHere[i - 1]);
+        }
+        return max;
     }
+
 
     /**
      *
