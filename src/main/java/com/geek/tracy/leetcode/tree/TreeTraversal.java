2d468c9c8dc2926bc07a9ac2803212183f97799c
tracy <709830862@qq.com>
2023-07-07 17:47:30 +0800

    树  中序遍历

--- src/main/java/com/geek/tracy/leetcode/tree/TreeTraversal.java
+++ src/main/java/com/geek/tracy/leetcode/tree/TreeTraversal.java
@@ -43,15 +43,16 @@ public class TreeTraversal implements TreeTraversalWay {
      */
     public List<Integer> preOrderIteration(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        // 使用队列，FIFO，进队顺序：右、左，前序遍历保持左节点在队首
+        // 使用栈，进栈顺序：右、左，前序遍历保持左节点在栈顶
         Deque<TreeNode> deque = new LinkedList<>();
         // 初始状态，队列只有根节点root
         deque.offerFirst(root);
         while (!deque.isEmpty()) {
             TreeNode queueHead = deque.pollFirst();
             if (queueHead != null) {
+                // 先获取根节点数据
                 result.add(queueHead.val);
-                // 进队顺序：右、左，前序遍历保持左节点在队首
+                // 后压栈，压栈顺序：右、左，前序遍历保持左节点在栈顶
                 deque.offerFirst(queueHead.right);
                 deque.offerFirst(queueHead.left);
             }
@@ -87,7 +88,22 @@ public class TreeTraversal implements TreeTraversalWay {
      * @param root 树节点
      */
     public List<Integer> inOrderIteration(TreeNode root) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+        // 使用栈
+        Deque<TreeNode> deque = new LinkedList<>();
+        while (!deque.isEmpty() || root != null) {
+            // 前序遍历，左--根--右，节点指针指向左子节点
+            while (root != null) {
+                deque.offerFirst(root);
+                root = root.left;
+            }
+            // 此时栈顶元素，即为最左子节点，访问之
+            root = deque.pollFirst();
+            result.add(root.val);
+            // 访问右子节点
+            root = root.right;
+        }
+        return result;
     }
 
     /**
@@ -112,4 +128,16 @@ public class TreeTraversal implements TreeTraversalWay {
         // 根
         list.add(tree.val);
     }
+
+    /**
+     * 后序遍历 -- 迭代，非递归
+     * @param root 树节点
+     */
+    public List<Integer> postOrderIteration(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        // 使用栈
+        Deque<TreeNode> deque = new LinkedList<>();
+        return result;
+    }
+
 }
