2f6a73bc499831706918f9a2cb05865176bcbf05
tracy <709830862@qq.com>
2023-07-14 19:49:14 +0800

    979. 在二叉树中分配硬币 distribute coins in binary tree

--- src/main/java/com/geek/tracy/leetcode/tree/CodeTreeSets.java
+++ src/main/java/com/geek/tracy/leetcode/tree/CodeTreeSets.java
@@ -13,6 +13,42 @@ import java.util.List;
 public class CodeTreeSets extends TreeTraversal {
 
     /**
+     * 题单：贡献法
+     * 891. 子序列宽度之和，题解
+     * 907. 子数组的最小值之和，题解
+     * 1856. 子数组最小乘积的最大值
+     * 2104. 子数组范围和
+     * 2681. 英雄的力量
+     * 2281. 巫师的总力量和
+     *
+     * 979. 在二叉树中分配硬币 distribute coins in binary tree
+     *
+     * 给定一个有 N 个结点的二叉树的根结点 root，树中的每个结点上都对应有 node.val 枚硬币，并且总共有 N 枚硬币。
+     * 在一次移动中，我们可以选择两个相邻的结点，然后将一枚硬币从其中一个结点移动到另一个结点。(移动可以是从父结点到子结点，或者从子结点移动到父结点。)。
+     * 返回使每个结点上只有一枚硬币所需的移动次数。
+     *
+     * 注：有点不好理解！！待进一步理解！
+     */
+    private int res = 0;
+    public int distributeCoins(TreeNode root) {
+        dfsCoin(root);
+        return res;
+    }
+
+    /**
+     * 深度优先算法
+     */
+    private int dfsCoin(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfsCoin(root.left);
+        int right = dfsCoin(root.right);
+        res += Math.abs(left) + Math.abs(right);
+        return left + right + root.val - 1;
+    }
+
+    /**
      * 144. 二叉树的前序遍历
      */
     public List<Integer> preorderTraversal(TreeNode root) {
