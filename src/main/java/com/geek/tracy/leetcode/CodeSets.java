c13913402a7a5878858de55cbde967a22169b391
tracy <709830862@qq.com>
2023-08-04 18:25:55 +0800

    722. 删除注释

--- src/main/java/com/geek/tracy/leetcode/CodeSets.java
+++ src/main/java/com/geek/tracy/leetcode/CodeSets.java
@@ -18,6 +18,40 @@ import java.util.Set;
  */
 public class CodeSets {
 
+
+    /**
+     * 722. 删除注释
+     */
+    public List<String> removeComments(String[] source) {
+        List<String> res = new ArrayList<String>();
+        StringBuilder newLine = new StringBuilder();
+        boolean inBlock = false;
+        for (String line : source) {
+            for (int i = 0; i < line.length(); i++) {
+                if (inBlock) {
+                    if (i + 1 < line.length() && line.charAt(i) == '*' && line.charAt(i + 1) == '/') {
+                        inBlock = false;
+                        i++;
+                    }
+                } else {
+                    if (i + 1 < line.length() && line.charAt(i) == '/' && line.charAt(i + 1) == '*') {
+                        inBlock = true;
+                        i++;
+                    } else if (i + 1 < line.length() && line.charAt(i) == '/' && line.charAt(i + 1) == '/') {
+                        break;
+                    } else {
+                        newLine.append(line.charAt(i));
+                    }
+                }
+            }
+            if (!inBlock && newLine.length() > 0) {
+                res.add(newLine.toString());
+                newLine.setLength(0);
+            }
+        }
+        return res;
+    }
+
     /**
      * 822. 翻转卡片游戏
      *
