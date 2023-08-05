61014958efeb89eb4da8c02dccc0f5a9269fcacc
Mike <709830862@qq.com>
2023-08-05 22:45:26 +0800

    21. 合并两个有序链表

--- src/main/java/com/geek/tracy/leetcode/linkedlist/ChainTable.java
+++ src/main/java/com/geek/tracy/leetcode/linkedlist/ChainTable.java
@@ -20,7 +20,52 @@ import java.util.stream.Collectors;
  */
 public class ChainTable {
 
+    @Test
+    public void mergeTwoListTest() {
+        ListNode list1 = ListNode.inti(1, 2, 4);
+        ListNode list2 = ListNode.inti(1, 3, 4);
+        ListNode merge = mergeTwoLists(list1, list2);
+        ListNode.printListNode(merge);
+    }
 
+    /**
+     * 21. 合并两个有序链表
+     * 思考：递归解法
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        Deque<ListNode> queue = new LinkedList<>();
+        while (list1 != null || list2 != null) {
+            if (list1 == null) {
+                queue.offerLast(list2);
+                list2 = list2.next;
+            } else if (list2 == null) {
+                queue.offerLast(list1);
+                list1 = list1.next;
+            } else {
+                if (list1.val > list2.val) {
+                    queue.offerLast(list2);
+                    list2 = list2.next;
+                } else {
+                    queue.offerLast(list1);
+                    list1 = list1.next;
+                }
+            }
+        }
+        ListNode head = queue.pollFirst();
+        ListNode curr = head;
+        while (!queue.isEmpty()) {
+            curr.next = queue.pollFirst();
+            curr = curr.next;
+        }
+        curr.next = null;
+        return head;
+    }
 
 
     @Test
