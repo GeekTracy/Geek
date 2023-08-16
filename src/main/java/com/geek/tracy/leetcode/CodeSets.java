04fd95f7060a4a13c508e894c02a77e3e94500eb
tracy <709830862@qq.com>
2023-08-16 17:39:04 +0800

    88. 合并两个有序数组

--- src/main/java/com/geek/tracy/leetcode/CodeSets.java
+++ src/main/java/com/geek/tracy/leetcode/CodeSets.java
@@ -18,6 +18,41 @@ import java.util.Set;
  */
 public class CodeSets {
 
+    @Test
+    public void mergeTest() {
+        int[] nums1 = new int[] {1,2,3,0,0,0} ;
+        int[] nums2 = new int[] {2,5,6} ;
+        merge(nums1, 3, nums2, 3);
+
+        int[] nums11 = new int[] {1} ;
+        int[] nums22 = new int[] {} ;
+        merge(nums11, 1, nums22, 0);
+
+        int[] nums111 = new int[] {0} ;
+        int[] nums222 = new int[] {1} ;
+        merge(nums111, 0, nums222, 1);
+        System.out.println();
+    }
+
+    /**
+     * 88. 合并两个有序数组
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        while (m > 0 || n > 0) {
+            if (m == 0) {
+                nums1[n - 1] = nums2[n - 1];
+                n--;
+            } else if (n == 0) {
+                m--;
+            } else  if (nums1[m - 1] > nums2[n - 1]) {
+                nums1[m + n - 1] = nums1[m - 1];
+                m--;
+            } else {
+                nums1[m + n - 1] = nums2[n - 1];
+                n--;
+            }
+        }
+    }
 
     /**
      * 2682. 找出转圈游戏输家
