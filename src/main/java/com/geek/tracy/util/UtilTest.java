c09fb166f7a53df9274f198e0c57fd654b36c805
tracy <709830862@qq.com>
2024-05-15 19:52:45 +0800

    最大收益

--- src/main/java/com/geek/tracy/util/UtilTest.java
+++ src/main/java/com/geek/tracy/util/UtilTest.java
@@ -1,6 +1,5 @@
 package com.geek.tracy.util;
 
-import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -11,6 +10,96 @@ import java.util.List;
  * @Date 2023/10/12
  */
 public class UtilTest {
+
+
+    /**
+     * 最大收益，计算dp[]数组，本体6块土地总油耗：25，
+     *
+     */
+    public int[] maxIncome2() {
+        // 土地二维数组，第一列表示油耗，第二列表示收益
+        int[][] land = new int[][]{{1, 1},{7, 9},{6, 10},{2, 4},{3, 5},{6, 10}};
+        // 所有土地总油耗
+        int oilSum = Arrays.stream(land).mapToInt(item -> item[0]).sum();
+        // 定义dp数组，下标表示油耗，值表示收益，其中下标为0值也为0
+        int[] dp = new int[oilSum + 1];
+        // dp[n] = Max(dp[n-1] + dp[1], land[n])
+        for (int i = 1; i < dp.length; i++) {
+
+        }
+        return new int[0];
+    }
+
+    @Test
+    public void testMaxIncome() {
+        maxIncome2();
+        for (int i = 0; i < 30; i++) {
+            System.out.println("总油耗：" + i + ", 最大收益" + maxIncome(i));
+        }
+    }
+
+
+    private static Integer MaxIncome = 0;
+    /**
+     * 回溯法，遍历所有的可能情况，算出汽车油耗范围内最大的收益
+     *
+     * @param oil 汽车油量
+     */
+    public int maxIncome(int oil) {
+        // 土地二维数组，第一列表示油耗，第二列表示收益
+        int[][] land = new int[][]{{1, 1},{7, 9},{6, 10},{2, 4},{3, 5},{6, 10}};
+        // 所有土地总油耗
+        int landOilSum = Arrays.stream(land).mapToInt(item -> item[0]).sum();
+        // 汽车耗油量大于等于所有土地油耗，活全干了收益最大
+        if (oil >= landOilSum) {
+            return Arrays.stream(land).mapToInt(item -> item[1]).sum();
+        }
+        // 按油耗升序排序
+        Arrays.sort(land, Comparator.comparingInt(item -> item[0]));
+        // 回溯法遍历所有土地的组合，使油耗小于等于汽车油耗时的最大收益
+        backSource(new ArrayList<>(), landOilSum, oil, land);
+        return MaxIncome;
+    }
+
+    /**
+     *
+     * @param landNum
+     * @param landOilSum
+     * @param land
+     * @return
+     */
+    private void backSource(List<Integer> landNum, Integer landOilSum, int carOilSum, int[][] land) {
+        // 当前油耗
+        int currOil = landNum.stream().mapToInt(index -> land[index][0]).sum();
+        // 剩下的油
+        int carLeftOil = carOilSum - currOil;
+        if (carLeftOil == 0) {
+            // 计算收益
+            MaxIncome = Math.max(MaxIncome, landNum.stream().mapToInt(index -> land[index][1]).sum());
+            return;
+        }
+        if (carLeftOil > 0) {
+            boolean firstOne = true;
+            // 遍历未使用的土地
+            for (int i = 0; i < land.length; i++) {
+                if (landNum.contains(i)) {
+                    continue;
+                } else if (firstOne && land[i][0] > carLeftOil) {
+                    // 遍历时第一个就大于车剩下的油量，已不再有可处理的地，直接返回
+                    // 计算收益
+                    MaxIncome = Math.max(MaxIncome, landNum.stream().mapToInt(index -> land[index][1]).sum());
+                    return;
+                } else if (land[i][0] <= carLeftOil) {
+                    firstOne = false;
+                    // 增加处理土地，往下钻
+                    landNum.add(i);
+                    backSource(landNum, landOilSum, carOilSum, land);
+                    // 回退
+                    landNum.remove(landNum.size() - 1);
+                }
+            }
+        }
+    }
 
     @Test
     public void testDecode() {
@@ -81,6 +170,9 @@ public class UtilTest {
         return f1;
     }
 
+    /**
+     * list 中解码的每一个字符拼接最后一个或两个字符映射的编码字符，若cha为空，则将返回空
+     */
     private List<String> merge(List<String> list, String cha) {
         if (null == cha || "".equals(cha)){
             return new ArrayList<>(0);
