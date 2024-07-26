1fece766fd0530e8f053d6baa9c11fcdfbbd621f
Mike <709830862@qq.com>
2024-07-26 12:01:18 +0800

    2740.找出分区值

--- src/main/java/com/geek/tracy/leetcode/CodeSets.java
+++ src/main/java/com/geek/tracy/leetcode/CodeSets.java
@@ -17,6 +17,32 @@ import java.util.stream.Collectors;
  */
 public class CodeSets {
 
+    /**
+     * 2740.找出分区值
+     *
+     * 给你一个 正 整数数组 nums 。
+     *
+     * 将 nums 分成两个数组：nums1 和 nums2 ，并满足下述条件：
+     *
+     *  1）数组 nums 中的每个元素都属于数组 nums1 或数组 nums2 。
+     *  2）两个数组都 非空 。
+     *  3）分区值 最小 。
+     * 分区值的计算方法是 |max(nums1) - min(nums2)| 。
+     *
+     * 其中，max(nums1) 表示数组 nums1 中的最大元素，min(nums2) 表示数组 nums2 中的最小元素。
+     *
+     * 返回表示分区值的整数。
+     */
+    public int findValueOfPartition(int[] nums) {
+        int prt = Integer.MAX_VALUE;
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            prt = Math.min(prt, nums[i] - nums[i - 1]);
+        }
+        return prt;
+    }
+
+
     @Test
     public void test_2844() {
         // 输入：num = "2245047"
